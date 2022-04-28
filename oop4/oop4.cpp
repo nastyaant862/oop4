7c3f27e9013601986ada297a5082f1f71ac1567e
Nastya A <Nastya.Antonova862@mail.ru>
2022-04-28 21:13:14 +0300

    Добавьте файлы проекта.

---
+++ oop4/oop4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a = -0.6;
+    double b = 5.3;
+    double z, t;
+
+    if (a < b) {
+        z = sqrt(abs(a * a - b * b));
+    }
+    else {
+        z = 1 - 2 * cos(a) * sin(b);
+    }
+
+    if (z < b) {
+        t = pow((z + a * a * b), 1 / 3);
+    }
+    else if (z = b) {
+        t = 1 - log10(z) + cos(a * a * b);
+    }
+    else {
+        t = 1 / (cos(z * a));
+    }
+
+    cout << a << endl << b << endl << z << endl << t;
+}
